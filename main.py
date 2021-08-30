6b185af7cc41d70a832d443e2205b3b0130b236a
Justyna Korzycka <justyna.korzycka@gmail.com>
2021-08-30 21:27:00 +0200

    Change of def of classes and add inheritance to main class.

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import json, os
 import requests
 import datetime
 import os.path
+import sys
 
 class FileMixin:
-  def __init__(self, file_name_):
-    self.file_name = file_name_
 
   def load_data_to_doc(self, record):
     with open(self.file_name, "a+") as file:
@@ -18,18 +17,16 @@ class FileMixin:
         for line in file:
           if len(line)>1:
             converted_data = json.loads(line)
-            if converted_data['location']['name'] == city and request_date in converted_data['location']['localtime']:
+            converted_date = datetime.datetime.strptime(converted_data['location']['localtime'], "%Y-%m-%d %H:%M")
+            if converted_data['location']['name'] == city and request_date == converted_date.date():
               return converted_data
         else:
           return False
 
 class ConectionMixin:
-  def __init__(self, api_key_):
-    self.url = 'http://api.weatherapi.com/v1/current.json?'
-    self.api_key = api_key_
 
-  def connect_with_api(self, city_name):
-    payload = {'key': self.api_key, 'q':{city_name}}
+  def connect_with_api(self, location):
+    payload = {'key': self.api_key, 'q':{location}}
     try:
       response = requests.get(self.url, params=payload)
     except requests.exceptions.RequestException as e:
@@ -38,22 +35,22 @@ class ConectionMixin:
     weather_data = response.json()
     return weather_data
 
-class WeatherAPI:
-  def __init__(self, file, api_key):
-    self.record = None
-    self.file = file
+class WeatherAPI(FileMixin, ConectionMixin):
+  def __init__(self, file_name, api_key):
+    self.file_name = file_name
     self.api_key = api_key
+    self.url = 'http://api.weatherapi.com/v1/current.json?'
   
   def get_data(self, location):
-    current_date = str(datetime.date.today())
-    self.record = self.file.check_and_get_record(current_date, location)
+    current_date = (datetime.date.today())
+    self.record = self.check_and_get_record(current_date, location)
+
     if not self.record:
-      connectionWIthApi =  ConectionMixin(self.api_key)
-      self.record = connectionWIthApi.connect_with_api(location)
+      self.record = self.connect_with_api(location)
       if self.record.get('error'):
         print(f'Błąd: {self.record}')
-        return 0
-      self.file.load_data_to_doc(self.record)
+        return 
+      self.load_data_to_doc(self.record)
     self.print_weather_data()
   
   def print_weather_data(self):
@@ -64,15 +61,12 @@ class WeatherAPI:
     cloud = self.record['current']['cloud']
     wind_speed = self.record['current']['wind_kph']
     pressure = self.record['current']['pressure_mb']
-    # chance_of_rain = self.record['current']['chance_of_rain']
-    # chance_of_snow = self.record['current']['chance_of_snow']
     print(f'Miasto: {city}\n Czas: {cur_date} \n Temperatura: {temp}\n Wilgotność powietrza: {humidity}%\n Zachmurzenie: {cloud}% \n Prędkość wiatru: {wind_speed}km/h \n Ciśnienie: {pressure} mbar \n ')
 
 def main():
-  api_key = 'c52833b4ef704f9e8f3151342211808'
+  api_key = sys.argv[1]
   file_name = 'weatherData.txt'
-  file_manager = FileMixin(file_name)
-  weatherData = WeatherAPI(file_manager, api_key)
+  weatherData = WeatherAPI(file_name, api_key)
   while True:
     location = input('Wpisz miasto: ')
     if location == '0':
